710ff1c95e5d1b09839130abfdb573181982f5fe
Yang Zhao <yzhao@csod.com>
2020-07-22 13:11:32 +1200

    Update google analytics

--- publishconf.py
+++ publishconf.py
@@ -21,4 +21,4 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 DISQUS_SITENAME = "yangtalks"
-GOOGLE_ANALYTICS = "UA-84962714-1"
+GOOGLE_ANALYTICS = "UA-84962714-5"
